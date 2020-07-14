6b5e4894648bb731e3b873686bd743aa16f13583
sarfojoe <60213446+sarfojoe@users.noreply.github.com>
2020-07-14 18:58:39 +0300

    input  button added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState(['Take dog for a walk', 'Take the rabbish out']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (event) => {
+    //this will fire off when we click the button
+    event.preventDefault(); //will stop the refresh
+    setTodos([...todos, input]);
+    setInput('');   //clear up the input after hitting the todo button
+
+  }
   return (
     <div className="App">
      <h1>Hello world</h1>
-     <input />
-     <button>Add todo</button>
+     <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <Button type='submit' onClick={addTodo} variant="contained" color="primary">Add Todo</Button>
+
+        {/* <button type='submit' onClick={addTodo}>Add todo</button> */}
+     </form>
+     
 
      <ul>
-       <li></li>
-       <li></li>
+       {todos.map(todo => (
+         <li>{todo}</li>
+       ))}
      </ul>
     </div>
   );
